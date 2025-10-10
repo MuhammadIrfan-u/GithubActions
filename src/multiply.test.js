c7eb6df896ddf8f331abeee890fc32c5d4aba491
unknown <irfanjunego332211@gmail.com>
2025-10-10 12:18:55 +0500

    Pass all test cases

--- src/multiply.test.js
+++ src/multiply.test.js
@@ -2,7 +2,7 @@
 const { multiply } = require('./multiply');
 
   test('multiply two numbers', () => {
-    expect(multiply(2, 3)).toBe(4);
+    expect(multiply(2, 3)).toBe(6);
     expect(multiply(-2, 4)).toBe(-8);
   });
 
